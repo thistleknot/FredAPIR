5daf394f45770ba37caba1bd0ddaa0a2b84940d2
This is... DWARF FORTRESS! <laferrierejc@gmail.com>
2018-05-02 20:00:26 -0700

    Reduce attempts
    New method to download data introduced by stackoverflow member Gregor in hopes of utilizing the reduce function, but as of yet, still not working.  Going to try other methods

--- SemanticFilter.R
+++ SemanticFilter.R
@@ -1,3 +1,5 @@
+library(data.table)
+
 semanticList = c("Population", "Price", "Employment","Consumer", "500", "Monetary Base", "Real", "Money Stock", "Treasury",  "Spread")
 
 semanticScore=77
@@ -78,8 +80,86 @@ for (i in parsedList)
 
   require(ggplot2)
   #print(dt)
-  write.csv(dt, file = parsedList[a])
-  #qplot(data = dt, x = date, y = value, geom = 'line')
+  #write.csv(dt, file = parsedList[a])
+  qplot(data = dt, x = date, y = value, geom = 'line')
 
   a=a+1
 }
+
+dt3<-c()
+dt4<-c()
+dt5<-c()
+
+print(length(parsedList))
+
+#join by date
+a=1
+for (i in parsedList)
+{
+
+  {
+    test <- fred$series.observations(series_id = parsedList[a], observation_start = "2000-01-01", observation_end = "2018-03-01")
+
+    test %>>%
+      select(
+        date,
+        value
+      ) %>>%
+      mutate(
+        date = as.Date(date),
+        value = as.numeric(value)
+      ) ->
+      dt[a]
+
+    #use merge
+
+  }
+  a=a+1
+  #break
+
+}
+
+#https://stackoverflow.com/questions/50118593/r-join-all-datasets-by-date/50139902#50139902
+# first download all the data    
+#by Gregor
+data_list = lapply(parsedList, function(a)
+  fred$series.observations(
+    series_id = a,
+    observation_start = "2000-01-01",
+    observation_end = "2018-03-01"
+  )
+)
+
+#use a column join function to inject a layer of rows for names?
+a=1
+for (i in parsedList)
+{
+  print(parsedList[a])
+  print(data_list[a])
+  a=a+1
+}
+
+# define function to process the data
+# we plan on re-naming the "value" column so each one is distinct
+process_data = function(d, value_name) {
+  d = d[, c("date", "value")]
+  d$date = as.Date(d$date)
+  d$value = as.numeric(d$value)
+  names(d)[2] = value_name
+  return(d)
+}
+
+# process the data
+data_list_processed = list()
+for (i in seq_along(data_list)) {
+  data_list_processed[[i]] = process_data(data_list[[i]], value_name = paste0("value", i))
+  print(process_data(data_list[[i]], value_name = paste0("value", i)))
+  #combined_data = Reduce(merge, data_list_processed)
+}
+
+# merge the data
+combined_data = Reduce(merge, data_list_processed)
+
+#print(data_list_processed)
+
+print(combined_data)
