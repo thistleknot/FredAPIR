f19e9a537933960a82005971947354fb6976b487
This is... DWARF FORTRESS! <laferrierejc@gmail.com>
2018-05-15 18:08:52 -0700

    Removed SP500
    due to the nature of the code, I didn't read the usage rights on what was scraped.  Consequently I suspect someone noticed the factor.  Hrmm... I removed it.  Consequently I imagine to get better results having access to a paid API would be preferred.

--- SemanticFilter.R
+++ SemanticFilter.R
@@ -57,18 +57,18 @@ for (i in semanticList)
 {
   y=1
   count=1
-
+  
   #load up data
   semantic<-fred$series.search(semanticList[x])
-
+  
   #loop through popularity scores
   #print(semanticList[x])
   for (y in semantic$popularity)
   {
     #if specific score is greater than 77, capture
-
+    
     #as.numeric(as.character(semantic$popularity[count]))
-
+    
     #https://stackoverflow.com/questions/2288485/how-to-convert-a-data-frame-column-to-numeric-type
     #if((semantic$popularity[count])>77)
     if((as.numeric(as.character(semantic$popularity[count])))>semanticScore)
@@ -76,25 +76,25 @@ for (i in semanticList)
       {
         #print("above")
         #print(semantic$id[count])
-
+        
         #print(semantic$popularity[count])
-
+        
         names<-c(names,semantic$id[count])
         popularityScores<-c(popularityScores,semantic$popularity[count])
-
+        
       }
-
+      
     }
     else
     {
-
+      
     }
-
+    
     count=count+1
   }
-
+  
   x=x+1
-
+  
 }
 
 parsedList<-unique(names)
@@ -123,13 +123,16 @@ print(parsedList)
 #parsedList<-c("DCOILWTICO","DGS30","T5YIE","DGS5","UMCSENT","SP500","TEDRATE","DGS1MO","T10Y2Y","M1","T5YIFR","IC4WSA","ICSA","VIXCLS","BAA10Y","BASE","DGS1","A191RL1Q225SBEA","INTDSRUSM193N","TCU","CIVPART","UNRATE","GS10","USSLIND","BAMLH0A3HYC","PSAVERT","DFF","TB3MS","T10Y3M","GOLDAMGBD228NLBM","WPU0911","RECPROUSM156N","PPIACO")
 
 #newGold
-  parsedList<-c("A191RL1Q225SBEA","BAA10Y","BASE","DCOILBRENTEU","DFF","DGS1","FPCPITOTLZGUSA","GS10","IC4WSA","ICSA","INTDSRUSM193N","MPRIME","PSAVERT","SP500","STLFSI","TCU","TEDRATE","UMCSENT","UNRATE","USSLIND","GOLDAMGBD228NLBM")
+#SP500 is copyrighted and was throwing errors when included.
+#parsedList<-c("A191RL1Q225SBEA","BAA10Y","BASE","DCOILBRENTEU","DFF","DGS1","FPCPITOTLZGUSA","GS10","IC4WSA","ICSA","INTDSRUSM193N","MPRIME","PSAVERT","SP500","STLFSI","TCU","TEDRATE","UMCSENT","UNRATE","USSLIND","GOLDAMGBD228NLBM")
+parsedList<-c("A191RL1Q225SBEA","BAA10Y","BASE","DCOILBRENTEU","DFF","DGS1","FPCPITOTLZGUSA","GS10","IC4WSA","ICSA","INTDSRUSM193N","MPRIME","PSAVERT","STLFSI","TCU","TEDRATE","UMCSENT","UNRATE","USSLIND","GOLDAMGBD228NLBM")
 
 print(parsedList)
 
 #https://stackoverflow.com/questions/50118593/r-join-all-datasets-by-date/50139902#50139902
 # first download all the data    
 #by Gregor 2018-05-2
+#troubleshoot series with this function
 data_list = lapply(parsedList, function(a)
   fred$series.observations(
     series_id = a,
@@ -137,6 +140,10 @@ data_list = lapply(parsedList, function(a)
     observation_end = end_date
   )
 )
+
+
+
+#print(parsedList[14])
 
 # define function to process the data
 # we plan on re-naming the "value" column so each one is distinct
@@ -148,13 +155,14 @@ process_data = function(d, value_name) {
   return(d)
 }
 
+
 # process the data
 data_list_processed = list()
 for (i in seq_along(data_list)) {
   
   #apply names
   data_list_processed[[i]] = process_data(data_list[[i]], value_name = parsedList[i])
-
+  
 }
 
 #merge data by date
@@ -251,7 +259,7 @@ count=length(parsedList)+1
 a=1
 for (i in 1:count)
 {
-
+  
   #naming
   print(i)
   print(a)
@@ -260,7 +268,7 @@ for (i in 1:count)
   #need to loop down to minLag
   names(past) <- c( paste(names(new[a]), "-1"), paste(names(new[a]), "-2") ,paste(names(new[a]), "-3"),paste(names(new[a]), "-4"),paste(names(new[a]), "-5"))
   names(new[a])
-
+  
   past2=data.frame(past)
   
   #join
@@ -273,7 +281,7 @@ for (i in 1:count)
   {
     past3<-cbind(past3,past2)
   }
-
+  
   a=a+1
   
 } 
@@ -363,20 +371,20 @@ for(i in 1:numLoops)
   #i=25
   print(i)
   
-  dataSet <- c()
-  futureSet <- c()
-  presentSet <- c()
+  #wdataSet <- c()
+  #futureSet <- c()
+  #presentSet <- c()
   
   #iterate here
   
-    #get next row (i.e. present)
+  #get next row (i.e. present)
   
   #data model
   #everything
-    #(tail(new3, -5))
+  #(tail(new3, -5))
   
   #data model with first windowSize # of elements
-  dataSet <- head(tail(new3, -4-i), windowSize)
+  wdataSet <- head(tail(new3, -4-i), windowSize)
   
   #input
   #single last record of windowSize+1
@@ -388,44 +396,44 @@ for(i in 1:numLoops)
   presentSet <- tail(head(tail(new3, -4-i), windowSize),1)
   
   #past 6 quarters
-    #testd <- lm(future ~ A191RL1Q225SBEA + BAA10Y + BASE + DCOILBRENTEU + DFF + DGS1 + FPCPITOTLZGUSA + GS10 + IC4WSA + ICSA + INTDSRUSM193N + MPRIME + PSAVERT + SP500 + STLFSI + TCU + TEDRATE + UMCSENT + UNRATE + USSLIND + GOLDAMGBD228NLBM + A191RL1Q225SBEA..1 + A191RL1Q225SBEA..2 + A191RL1Q225SBEA..3 + A191RL1Q225SBEA..4 + A191RL1Q225SBEA..5 + BAA10Y..1 + BAA10Y..2 + BAA10Y..3 + BAA10Y..4 + BAA10Y..5 + BASE..1 + BASE..2 + BASE..3 + BASE..4 + BASE..5 + DCOILBRENTEU..1 + DCOILBRENTEU..2 + DCOILBRENTEU..3 + DCOILBRENTEU..4 + DCOILBRENTEU..5 + DCOILBRENTEU..1 + DCOILBRENTEU..2 + DCOILBRENTEU..3 + DCOILBRENTEU..4 + DCOILBRENTEU..5 + DFF..1 + DFF..2 + DFF..3 + DFF..4 + DFF..5 + DGS1..1 + DGS1..2 + DGS1..3 + DGS1..4 + DGS1..5 + FPCPITOTLZGUSA..1 + FPCPITOTLZGUSA..2 + FPCPITOTLZGUSA..3 + FPCPITOTLZGUSA..4 + FPCPITOTLZGUSA..5 + GS10..1 + GS10..2 + GS10..3 + GS10..4 + GS10..5 + IC4WSA..1 + IC4WSA..2 + IC4WSA..3 + IC4WSA..4 + IC4WSA..5 + ICSA..1 + ICSA..2 + ICSA..3 + ICSA..4 + ICSA..5 + INTDSRUSM193N..1 + INTDSRUSM193N..2 + INTDSRUSM193N..3 + INTDSRUSM193N..4 + INTDSRUSM193N..5 + MPRIME..1 + MPRIME..2 + MPRIME..3 + MPRIME..4 + MPRIME..5 + PSAVERT..1 + PSAVERT..2 + PSAVERT..3 + PSAVERT..4 + PSAVERT..5 + SP500..1 + SP500..2 + SP500..3 + SP500..4 + SP500..5 + STLFSI..1 + STLFSI..2 + STLFSI..3 + STLFSI..4 + STLFSI..5 + TCU..1 + TCU..2 + TCU..3 + TCU..4 + TCU..5 + TEDRATE..1 + TEDRATE..2 + TEDRATE..3 + TEDRATE..4 + TEDRATE..5 + UMCSENT..1 + UMCSENT..2 + UMCSENT..3 + UMCSENT..4 + UMCSENT..5 + UNRATE..1 + UNRATE..2 + UNRATE..3 + UNRATE..4 + UNRATE..5 + USSLIND..1 + USSLIND..2 + USSLIND..3 + USSLIND..4 + USSLIND..5 + GOLDAMGBD228NLBM..1 + GOLDAMGBD228NLBM..2 + GOLDAMGBD228NLBM..3 + GOLDAMGBD228NLBM..4 + GOLDAMGBD228NLBM..5,  data = head(tail(new3, -5), 150))
+  #testd <- lm(future ~ A191RL1Q225SBEA + BAA10Y + BASE + DCOILBRENTEU + DFF + DGS1 + FPCPITOTLZGUSA + GS10 + IC4WSA + ICSA + INTDSRUSM193N + MPRIME + PSAVERT + SP500 + STLFSI + TCU + TEDRATE + UMCSENT + UNRATE + USSLIND + GOLDAMGBD228NLBM + A191RL1Q225SBEA..1 + A191RL1Q225SBEA..2 + A191RL1Q225SBEA..3 + A191RL1Q225SBEA..4 + A191RL1Q225SBEA..5 + BAA10Y..1 + BAA10Y..2 + BAA10Y..3 + BAA10Y..4 + BAA10Y..5 + BASE..1 + BASE..2 + BASE..3 + BASE..4 + BASE..5 + DCOILBRENTEU..1 + DCOILBRENTEU..2 + DCOILBRENTEU..3 + DCOILBRENTEU..4 + DCOILBRENTEU..5 + DCOILBRENTEU..1 + DCOILBRENTEU..2 + DCOILBRENTEU..3 + DCOILBRENTEU..4 + DCOILBRENTEU..5 + DFF..1 + DFF..2 + DFF..3 + DFF..4 + DFF..5 + DGS1..1 + DGS1..2 + DGS1..3 + DGS1..4 + DGS1..5 + FPCPITOTLZGUSA..1 + FPCPITOTLZGUSA..2 + FPCPITOTLZGUSA..3 + FPCPITOTLZGUSA..4 + FPCPITOTLZGUSA..5 + GS10..1 + GS10..2 + GS10..3 + GS10..4 + GS10..5 + IC4WSA..1 + IC4WSA..2 + IC4WSA..3 + IC4WSA..4 + IC4WSA..5 + ICSA..1 + ICSA..2 + ICSA..3 + ICSA..4 + ICSA..5 + INTDSRUSM193N..1 + INTDSRUSM193N..2 + INTDSRUSM193N..3 + INTDSRUSM193N..4 + INTDSRUSM193N..5 + MPRIME..1 + MPRIME..2 + MPRIME..3 + MPRIME..4 + MPRIME..5 + PSAVERT..1 + PSAVERT..2 + PSAVERT..3 + PSAVERT..4 + PSAVERT..5 + SP500..1 + SP500..2 + SP500..3 + SP500..4 + SP500..5 + STLFSI..1 + STLFSI..2 + STLFSI..3 + STLFSI..4 + STLFSI..5 + TCU..1 + TCU..2 + TCU..3 + TCU..4 + TCU..5 + TEDRATE..1 + TEDRATE..2 + TEDRATE..3 + TEDRATE..4 + TEDRATE..5 + UMCSENT..1 + UMCSENT..2 + UMCSENT..3 + UMCSENT..4 + UMCSENT..5 + UNRATE..1 + UNRATE..2 + UNRATE..3 + UNRATE..4 + UNRATE..5 + USSLIND..1 + USSLIND..2 + USSLIND..3 + USSLIND..4 + USSLIND..5 + GOLDAMGBD228NLBM..1 + GOLDAMGBD228NLBM..2 + GOLDAMGBD228NLBM..3 + GOLDAMGBD228NLBM..4 + GOLDAMGBD228NLBM..5,  data = head(tail(new3, -5), 150))
   
   #past 4 quarters to reduce # of columns < rows
-    windowModel <- lm(future ~ A191RL1Q225SBEA + BAA10Y + BASE + DCOILBRENTEU + DFF + DGS1 + FPCPITOTLZGUSA + GS10 + IC4WSA + ICSA + INTDSRUSM193N + MPRIME + PSAVERT + SP500 + STLFSI + TCU + TEDRATE + UMCSENT + UNRATE + USSLIND + GOLDAMGBD228NLBM + A191RL1Q225SBEA..1 + A191RL1Q225SBEA..2 + A191RL1Q225SBEA..3 + BAA10Y..1 + BAA10Y..2 + BAA10Y..3 + BASE..1 + BASE..2 + BASE..3 + DCOILBRENTEU..1 + DCOILBRENTEU..2 + DCOILBRENTEU..3 + DCOILBRENTEU..1 + DCOILBRENTEU..2 + DCOILBRENTEU..3 + DFF..1 + DFF..2 + DFF..3 + DGS1..1 + DGS1..2 + DGS1..3 + DGS1..4 + DGS1..5 + FPCPITOTLZGUSA..1 + FPCPITOTLZGUSA..2 + FPCPITOTLZGUSA..3 + GS10..1 + GS10..2 + GS10..3 + IC4WSA..1 + IC4WSA..2 + IC4WSA..3 + ICSA..1 + ICSA..2 + ICSA..3 + INTDSRUSM193N..1 + INTDSRUSM193N..2 + INTDSRUSM193N..3 + MPRIME..1 + MPRIME..2 + MPRIME..3 + PSAVERT..1 + PSAVERT..2 + PSAVERT..3 + SP500..1 + SP500..2 + SP500..3 + STLFSI..1 + STLFSI..2 + STLFSI..3 + TCU..1 + TCU..2 + TCU..3 + TEDRATE..1 + TEDRATE..2 + TEDRATE..3 + UMCSENT..1 + UMCSENT..2 + UMCSENT..3 + UNRATE..1 + UNRATE..2 + UNRATE..3 + USSLIND..1 + USSLIND..2 + USSLIND..3 + GOLDAMGBD228NLBM..1 + GOLDAMGBD228NLBM..2 + GOLDAMGBD228NLBM..3,  data = dataSet)
+  #sp500 is copyrighted
+  windowModel <- lm(future ~ A191RL1Q225SBEA + BAA10Y + BASE + DCOILBRENTEU + DFF + DGS1 + FPCPITOTLZGUSA + GS10 + IC4WSA + ICSA + INTDSRUSM193N + MPRIME + PSAVERT + STLFSI + TCU + TEDRATE + UMCSENT + UNRATE + USSLIND + GOLDAMGBD228NLBM + A191RL1Q225SBEA..1 + A191RL1Q225SBEA..2 + A191RL1Q225SBEA..3 + BAA10Y..1 + BAA10Y..2 + BAA10Y..3 + BASE..1 + BASE..2 + BASE..3 + DCOILBRENTEU..1 + DCOILBRENTEU..2 + DCOILBRENTEU..3 + DCOILBRENTEU..1 + DCOILBRENTEU..2 + DCOILBRENTEU..3 + DFF..1 + DFF..2 + DFF..3 + DGS1..1 + DGS1..2 + DGS1..3 + DGS1..4 + DGS1..5 + FPCPITOTLZGUSA..1 + FPCPITOTLZGUSA..2 + FPCPITOTLZGUSA..3 + GS10..1 + GS10..2 + GS10..3 + IC4WSA..1 + IC4WSA..2 + IC4WSA..3 + ICSA..1 + ICSA..2 + ICSA..3 + INTDSRUSM193N..1 + INTDSRUSM193N..2 + INTDSRUSM193N..3 + MPRIME..1 + MPRIME..2 + MPRIME..3 + PSAVERT..1 + PSAVERT..2 + PSAVERT..3 + STLFSI..1 + STLFSI..2 + STLFSI..3 + TCU..1 + TCU..2 + TCU..3 + TEDRATE..1 + TEDRATE..2 + TEDRATE..3 + UMCSENT..1 + UMCSENT..2 + UMCSENT..3 + UNRATE..1 + UNRATE..2 + UNRATE..3 + USSLIND..1 + USSLIND..2 + USSLIND..3 + GOLDAMGBD228NLBM..1 + GOLDAMGBD228NLBM..2 + GOLDAMGBD228NLBM..3,  data = wdataSet)
   
-    summary(windowModel)$adj.r.squared
-    
-    #p2 <- plot(testd,2)
-    
-    #plot futures
-    plot(GOLDAMGBD228NLBM ~ date, data=dataSet)
-    
-    #fit predicted model
-    
-    #plot two graphs per model
-    #https://stackoverflow.com/questions/2564258/plot-two-graphs-in-same-plot-in-r
-    #lines(windowModel$fitted.values ~ date, data=dataSet)
-    points(futureSet$date, predict(windowModel,futureSet), col=254)
-    
-    print("Current Present")
-    presentSet$GOLDAMGBD228NLBM
-    print("Current Future")
-    presentSet$future
-    #predict next
-    print("Next Present")
-    #tail(head(tail(new3$GOLDAMGBD228NLBM, -5), windowSize+1),1)
-    futureSet$GOLDAMGBD228NLBM
-    print("Next Future")
-    #tail(head(tail(new3$future, -5), windowSize+1),1)
-    futureSet$future
-    print("future set date")
-    print(futureSet$date)
-    print("90% Prediction Window Next Future")
-    predict(windowModel,futureSet,interval="predict",level=.90)
-    print('99% Prediction Window Next Future')
-    predict(windowModel,futureSet,interval="predict",level=.99)
+  summary(windowModel)$adj.r.squared
   
-    
+  #p2 <- plot(testd,2)
+  
+  #plot futures
+  plot(GOLDAMGBD228NLBM ~ date, data=wdataSet)
+  
+  #fit predicted model
+  
+  #plot two graphs per model
+  #https://stackoverflow.com/questions/2564258/plot-two-graphs-in-same-plot-in-r
+  #lines(windowModel$fitted.values ~ date, data=dataSet)
+  points(futureSet$date, predict(windowModel,futureSet), col=254)
+  
+  print("Current Present")
+  presentSet$GOLDAMGBD228NLBM
+  print("Current Future")
+  presentSet$future
+  #predict next
+  print("Next Present")
+  #tail(head(tail(new3$GOLDAMGBD228NLBM, -5), windowSize+1),1)
+  futureSet$GOLDAMGBD228NLBM
+  print("Next Future")
+  #tail(head(tail(new3$future, -5), windowSize+1),1)
+  futureSet$future
+  print("future set date")
+  print(futureSet$date)
+  print("90% Prediction Window Next Future")
+  predict(windowModel,futureSet,interval="predict",level=.90)
+  print('99% Prediction Window Next Future')
+  predict(windowModel,futureSet,interval="predict",level=.99)
+  
 }
 
 #next is to build flags for binary logistic regression
