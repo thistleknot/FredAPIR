3b2d9c234fddf9011b4bdd13eabc0c4e4311ca0b
thistleknot <laferrierejc@gmail.com>
2018-12-12 19:38:01 +0000

    I have the filtered subset selections skimmed for their factors and applied to the same training data for new coefficients.  this model is then applied to validation data (new data) and RMSE measured.
    Future direction: kfolds this section specifically (test).  Use new validation data to build training models off of, [use backwards step to create model from subset]

--- regression_analysis.R
+++ regression_analysis.R
@@ -101,7 +101,7 @@ resultsAll <- ols_step_all_possible(trainingModel, p=.05)
 resultsSubSet <- ols_step_best_subset(trainingModel, p=.05)
 
 #results <- ols_regress(trainingModel, p=.05)
-View(results)
+#View(resultsAll)
 
 #https://www.analyticsvidhya.com/blog/2018/05/improve-model-performance-cross-validation-in-python-r/
 #cross validation
@@ -119,8 +119,6 @@ View(results)
 cv.lm(test_xy_set, trainingModel, m=3) # 3 fold cross-validation
 
 cv.lm(train_xy_set, trainingModel, m=3) # 3 fold cross-validation
-
-
 
 #https://www.rdocumentation.org/packages/EnvStats/versions/2.3.1/topics/predict.lm
 #how to do this for test data?
@@ -176,71 +174,90 @@ plot(results)
 
 #https://dplyr.tidyverse.org/reference/filter.html
 
-# Mallows Distance from n Filter
-mcp <- round(results$cp-results$n,0)
-mcp_floor <- min(mean(mcp),median(mcp))
-subset1 <- filter(results, results$cp-results$n <= mcp_floor)
-#View(subset1)
+# Mallows Distance from n Filter, lower absolute distance from n is better
+#better to allow for more higher mallow's CP values since they are so disparate and no need to punish so heavily early on
 
+nrow(results)
+
+#filter
+#https://blog.exploratory.io/filter-data-with-dplyr-76cf5f1a258e
+
+mcp <- round(results$cp-results$n,0)
+mcp_floor <- max(mean(mcp),median(mcp))
+subset1 <- filter(results, results$cp-results$n <= mcp_floor)
 #adj R^2 filter
 adjRfilter <- max(mean(subset1$adjr),median(subset1$adjr))
 subset2 <- filter(subset1, subset1$adjr >= adjRfilter)
-
 #n filter
 sizefilter <- min(mean(subset2$n),median(subset2$n))
 subset3 <- filter(subset2, subset2$n <= sizefilter)
-View(subset3)
-
-#Erro Filter
+#Error Filter
 errorFilter <- min(mean(subset3$msep),median(subset3$msep))
-subset4 <- filter(subset3, subset3$msep <= errorFilter)
-
+subset4 <- filter(subset3, subset3$msep < errorFilter)
 #AIC Filter
 AICFilter <- min(mean(subset4$aic),median(subset4$aic))
-subset5 <- filter(subset4, subset4$aic <= AICFilter)
-View(subset5)
-
+subset5 <- filter(subset4, subset4$aic < AICFilter)
 #SBC and SBIC 
 SBICFilter <- min(mean(subset5$sbic),median(subset5$sbic))
 SBCFilter <- min(mean(subset5$sbc),median(subset5$sbc))
-subset6<- filter(subset5, subset5$sbc <= SBCFilter & subset5$sbic <= SBICFilter)
-View(subset6)
-
+subset6<- filter(subset5, subset5$sbc < SBCFilter & subset5$sbic <= SBICFilter)
 #APC Filter Higher is better
 #https://olsrr.rsquaredacademy.com/reference/ols_apc.html
 APCFilter <- max(mean(subset6$apc),median(subset6$apc))
-subset7<- filter(subset6, subset6$apc >= APCFilter)
-
+subset7<- filter(subset6, subset6$apc > APCFilter)
 #Hocking's SP, lower is better
 #https://rdrr.io/cran/olsrr/man/ols_hsp.html
 #Average prediction mean squared error
 HSPFilter <- min(mean(subset7$hsp),median(subset7$hsp))
-subset8<- filter(subset7, subset7$hsp <= HSPFilter)
-
+subset8<- filter(subset7, subset7$hsp < HSPFilter)
 #Final Prediction Error
 FPEFilter <- min(mean(subset8$fpe),median(subset8$fpe))
-subset9<- filter(subset8, subset8$fpe <= FPEFilter)
-
+subset9<- filter(subset8, subset8$fpe < FPEFilter)
+#filtered
 View(subset9)
+print(subset9)
 
-#plot(subset4)
+#best subselections
+print(resultsSubSet)
+View(resultsSubSet)
 
 #do an if check more than 1 row, if so, pick minimimum error
-if(nrow(subset4)>1) {
+if(nrow(subset9)>1) {
   #subset3 <- filter(subset3, subset3$msep == min(subset3$msep))
   print("yes")
-  View(subset4)
+  #View(subset4)
 } else {
   print("no")
-  View(subset4)
+  #View(subset4)
 }
 
-plot(subset4$n,subset4$adjr)
+plot(subset9$n,subset9$adjr)
 
-#jpeg('rplot.jpg')
-#ggplot(results)
-#dev.off()
-#break immediately
-#ols_step_backward_p(trainingModel, p=.1)
+#subset9$predictors[1]
 
-#colnames(merge(train_y,train_x))
+a=0
+for (i in subset9$predictors) {
+  factor_list <- c()
+  var <- c()
+  a=a+1
+  factor_list <- subset9$predictors[a]
+  vars <- scan(text = factor_list, what = "")
+  
+  names <- c()
+  
+  names <- c(names, 'yFYield_CSUSHPINSA')
+  
+  names <- c(names, vars)
+  
+  print(names)
+  
+  trainingModel <- lm(train_xy_set[names])
+  fit <- lm(trainingModel, data=test_xy_set[names])
+  
+  print(summary(fit))
+  print(RMSE(fit))
+  
+  #https://stackoverflow.com/questions/24741541/split-a-string-by-any-number-of-spaces
+  
+}
+
