9b788a36d6b0d4d1f9cad921e4a91dd611fcbe49
thistleknot <laferrierejc@gmail.com>
2018-12-13 00:56:39 +0000

    two lists merged

--- regression_analysis.R
+++ regression_analysis.R
@@ -24,6 +24,8 @@ PRESS <- function(linear.model) {
 
 MyData <- read.csv(file="prepped.csv", header=TRUE, sep=",")
 
+fieldOfInterest='yFYield_CSUSHPINSA'
+
 colnames(MyData) 
 
 nonXFields <- c('test2_z.date','BL_yFYield_CSUSHPINSA','yFYield_CSUSHPINSA')
@@ -57,7 +59,16 @@ smp_size <- floor(0.75 * nrow(MyData))
 set.seed(123)
 
 #possible to define two splits?
-train_ind <- sample(seq_len(nrow(x)), size = smp_size)
+train1_ind <- sample(seq_len(nrow(x)), size = smp_size)
+train2_ind <- sample(seq_len(nrow(x)), size = smp_size)
+
+#used for creating models
+valid1_ind <- sample(seq_len(nrow(x)), size = smp_size)
+
+#used for kfold bootstrap validation testing
+test1_ind <- sample(seq_len(nrow(x)), size = smp_size)
+
+#final RMSE should be evaluated against model that performs best against actual data.
 
 #training x,y
 #train_x <- x[train_ind, ]
@@ -70,24 +81,28 @@ train_ind <- sample(seq_len(nrow(x)), size = smp_size)
 #difference is -train
 #test_x <- x[-train_ind, ]
 #test_y <- y[-train_ind, ]
-test_xy_set <- MyData[-train_ind, c(xList,yField)]
+#test_xy_set <- MyData[-train_ind, c(xList,yField)]
+
+test1_xy_set <- MyData[test1_ind, c(xList,yField)]
 
 #http://r-statistics.co/Linear-Regression.html
-set.seed(100)  # setting seed to reproduce results of random sampling
+#set.seed(100)  # setting seed to reproduce results of random sampling
 
 #not limited to just the first portion of the percent
-trainingRowIndex <- sample(1:nrow(MyData), 0.6*nrow(MyData))  # row indices for training data
-training2RowIndex <- sample(1:nrow(MyData), 0.6*nrow(MyData))  # row indices for training data
+#training1RowIndex <- sample(1:nrow(MyData), 0.6*nrow(MyData))  # row indices for training data
+#training2RowIndex <- sample(1:nrow(MyData), 0.6*nrow(MyData))  # row indices for training data
 
 validationRowIndex <- sample(1:nrow(MyData), 0.6*nrow(MyData))  # row indices for training data
 testingRowIndex <- sample(1:nrow(MyData), 0.6*nrow(MyData))  # row indices for training data
 
-trainingData <- MyData[trainingRowIndex, ]  # model training data
-training2Data <- MyData[training2RowIndex, ]  # model training data
+training1Data <- MyData[train1_ind, ]  # model training data
+training2Data <- MyData[train2_ind, ]  # model training data
 
-train_xy_set <- trainingData[c(xList,yField)]
-train2_xy_set <- training2Data[c(xList,yField)]
+train1_xy_set <- training1Data[c(yField,xList)]
+train2_xy_set <- training2Data[c(yField,xList)]
+#View(train_xy_set)
 
+names <- c()
 vars <- c()
 names <- c(names, 'yFYield_CSUSHPINSA')
 #SPSSReducedModel <- c('yFYield_CSUSHPINSA','Q1','Q2','Q3','Q4','xYield_CASTHPI','xYield_CPALTT01USQ657N','xYield_GS10','xYield_MSPNHSUS','xYield_MVLOAS','xYield_NYXRSA','xYield_POP','xYield_POPTHM',xYield_SDXRSA','xYield_TB3MS','xYield_UMCSENT','xYield_USSLIND')
@@ -96,18 +111,90 @@ names <- c(names, 'yFYield_CSUSHPINSA')
 vars <- c('Q1','Q3','Q4','xYield_CASTHPI','xYield_CPALTT01USQ657N','xYield_GS10','xYield_MSPNHSUS','xYield_MVLOAS','xYield_NYXRSA','xYield_POPTHM','xYield_SDXRSA','xYield_TB3MS','xYield_UMCSENT','xYield_USSLIND')
 names <- c(names, vars)
 
-trainingModel <- lm(train_xy_set[names])
-training2Model <- lm(train2_xy_set[names])
+#doesn't throw an error here, but when I try to pass this model into ols_step_all_possible, it throws an error
+#training1Model <- lm(train1_xy_set[names])
+training1Model <- lm(yFYield_CSUSHPINSA ~ Q1 + Q3 + Q4 + xYield_CASTHPI + xYield_CPALTT01USQ657N + xYield_GS10 + xYield_MSPNHSUS + xYield_MVLOAS + xYield_NYXRSA + xYield_POPTHM + xYield_SDXRSA + xYield_TB3MS + xYield_UMCSENT + xYield_USSLIND, data = train1_xy_set)
 
-testingData  <- MyData[-trainingRowIndex, ]   # test data
+#training2Model <- lm(train2_xy_set[names])
+training2Model <- lm(yFYield_CSUSHPINSA ~ Q1 + Q3 + Q4 + xYield_CASTHPI + xYield_CPALTT01USQ657N + xYield_GS10 + xYield_MSPNHSUS + xYield_MVLOAS + xYield_NYXRSA + xYield_POPTHM + xYield_SDXRSA + xYield_TB3MS + xYield_UMCSENT + xYield_USSLIND, data = train2_xy_set)
 
-validationData  <- MyData[-trainingRowIndex, ]   # test data
+table(is.na(train1_xy_set[names]))
+table(is.na(train2_xy_set[names]))
+
+#testingData  <- MyData[-trainingRowIndex, ]   # test data
+
+#validationData  <- MyData[-trainingRowIndex, ]   # test data
 
 #merge quickly
 #total <- merge(data frameA,data frameB,by="ID")
 
-resultsAll <- ols_step_all_possible(trainingModel, p=.05)
-results2All <- ols_step_all_possible(training2Model, p=.05)
+summary(training1Model)
+summary(training2Model)
+
+#View(training2Model)
+#View(resultsAll)
+resultsAAll <- ols_step_all_possible(training1Model, p=.05)
+resultsAAll <- results1All
+resultsBAll <- ols_step_all_possible(training2Model, p=.05)
+resultsBAll <- results2All
+
+results2All$predictors
+
+#Adj R^2 Filter
+adjR_Afilter <- max(mean(resultsAAll$adjr),median(resultsAAll$adjr))
+adjR_Bfilter <- max(mean(resultsBAll$adjr),median(resultsBAll$adjr))
+
+#Mallows Filter
+mcp_A_floor <- min(mean(resultsAAll$cp-resultsAAll$n),median(resultsAAll$cp-resultsAAll$n))
+mcp_B_floor <- min(mean(resultsBAll$cp-resultsBAll$n),median(resultsBAll$cp-resultsBAll$n))
+
+#size filter
+size_A_floor <- min(mean(resultsAAll$n),median(resultsAAll$n))
+size_B_floor <- min(mean(resultsBAll$n),median(resultsBAll$n))
+
+#Error Filter
+error_AFilter <- min(mean(resultsAAll$msep),median(resultsAAll$msep))
+error_BFilter <- min(mean(resultsBAll$msep),median(resultsBAll$msep))
+
+#AIC Filter
+AIC_AFilter <- min(mean(resultsAAll$aic),median(resultsAAll$aic))
+AIC_BFilter <- min(mean(resultsBAll$aic),median(resultsBAll$aic))
+
+#SBC and SBIC 
+SBIC_AFilter <- min(mean(resultsAAll$sbic),median(resultsAAll$sbic))
+SBC_AFilter <- min(mean(resultsAAll$sbc),median(resultsAAll$sbc))
+SBIC_BFilter <- min(mean(resultsBAll$sbic),median(resultsBAll$sbic))
+SBC_BFilter <- min(mean(resultsBAll$sbc),median(resultsBAll$sbc))
+
+#APC Filter Higher is better
+#https://olsrr.rsquaredacademy.com/reference/ols_apc.html
+#Amemiya's Prediction Criterion penalizes R-squared more heavily than does adjusted R-squared for each addition degree of freedom used on the right-hand-side of the equation. 
+#The higher the better for this criterion.
+APC_AFilter <- max(mean(resultsAAll$apc),median(resultsAAll$apc))
+APC_BFilter <- max(mean(resultsBAll$apc),median(resultsBAll$apc))
+#& (resultsAAll$apc > APCAFilter)
+#decisive, heavily penalizes per factor
+
+#Hocking's SP, lower is better
+#https://rdrr.io/cran/olsrr/man/ols_hsp.html
+HSP_AFilter <- min(mean(resultsAAll$hsp),median(resultsAAll$hsp))
+HSP_BFilter <- min(mean(resultsBAll$hsp),median(resultsBAll$hsp))
+
+#Average prediction mean squared error
+#Final Prediction Error
+FPE_AFilter <- min(mean(resultsAAll$fpe),median(resultsAAll$fpe))
+FPE_BFilter <- min(mean(resultsBAll$fpe),median(resultsBAll$fpe))
+
+#filtered
+#AIC and BIC hold the same interpretation in terms of model comparison. That is, the larger difference in either AIC or BIC indicates stronger evidence for one model over the other 
+#(the lower the better). It's just the the AIC doesn't penalize the number of parameters as strongly as BIC.Jan 7, 2014
+
+subsetA <- filter(resultsAAll,  (resultsAAll$msep < error_AFilter) & (resultsAAll$adjr > adjR_Afilter)  & ((resultsAAll$cp-resultsAAll$n) <= mcp_A_floor) & (resultsAAll$n < size_A_floor)& (resultsAAll$aic < AIC_AFilter) & (resultsAAll$sbc < SBC_AFilter) & (resultsAAll$sbic < SBIC_AFilter)  & (resultsAAll$hsp < HSP_AFilter) & (resultsAAll$fpe < FPE_AFilter))
+subsetB <- filter(resultsBAll,  (resultsBAll$msep < error_BFilter) & (resultsBAll$adjr > adjR_Bfilter)  & ((resultsBAll$cp-resultsBAll$n) <= mcp_B_floor) & (resultsBAll$n < size_B_floor)& (resultsBAll$aic < AIC_BFilter) & (resultsBAll$sbc < SBC_BFilter) & (resultsBAll$sbic < SBIC_BFilter)  & (resultsBAll$hsp < HSP_BFilter) & (resultsBAll$fpe < FPE_BFilter))
+
+factor_test_list <- intersect(subsetA$predictors,subsetB$predictors)
+View(factor_test_list)
+
 
 resultsSubSet <- ols_step_best_subset(trainingModel, p=.05)
 
@@ -127,33 +214,36 @@ resultsSubSet <- ols_step_best_subset(trainingModel, p=.05)
 #names(flds)[1] <- "train"
 
 #https://www.statmethods.net/stats/regression.html
-cv.lm(test_xy_set, trainingModel, m=3) # 3 fold cross-validation
+cv.lm(test1_xy_set, training1Model, m=3) # 3 fold cross-validation
 
-cv.lm(train_xy_set, trainingModel, m=3) # 3 fold cross-validation
+cv.lm(train1_xy_set, training1Model, m=3) # 3 fold cross-validation
 
 #https://www.rdocumentation.org/packages/EnvStats/versions/2.3.1/topics/predict.lm
 #how to do this for test data?
-traindistPred <- predict(trainingModel)
-plot(traindistPred,trainingModel$residuals)
+traindistPred <- predict(training1Model)
+plot(traindistPred,training1Model$residuals)
 
-nrow(test_xy_set)
+nrow(test1_xy_set)
 
 #studentized residuals
-olsrr::ols_plot_resid_stud_fit(trainingModel)
-hist(trainingModel$residuals)
-summary(trainingModel)
+olsrr::ols_plot_resid_stud_fit(training1Model)
+olsrr::ols_plot_resid_stud_fit(training2Model)
+hist(training1Model$residuals)
+hist(training2Model$residuals)
+summary(training1Model)
+summary(training2Model)
 
 #RMSE of training
-RMSE(trainingModel)
+RMSE(training1Model)
 
 #RSS
-PRESS(trainingModel)
+PRESS(training1Model)
 
-print(trainingModel)
-trainingModel$coefficients
+print(training1Model)
+training1Model$coefficients
 
 #http://r-statistics.co/Linear-Regression.html
-fit <- lm(trainingModel, data=test_xy_set)
+fit <- lm(training1Model, data=test1_xy_set)
 testdistPred <- predict(fit)
 plot(testdistPred,fit$residuals)
 #https://www.rdocumentation.org/packages/DescTools/versions/0.99.19/topics/Measures%20of%20Accuracy
@@ -193,38 +283,7 @@ nrow(results)
 #filter
 #https://blog.exploratory.io/filter-data-with-dplyr-76cf5f1a258e
 
-mcp <- round(results$cp-results$n,0)
-mcp_floor <- max(mean(mcp),median(mcp))
-subset1 <- filter(results, results$cp-results$n <= mcp_floor)
-#adj R^2 filter
-adjRfilter <- max(mean(subset1$adjr),median(subset1$adjr))
-subset2 <- filter(subset1, subset1$adjr >= adjRfilter)
-#n filter
-sizefilter <- min(mean(subset2$n),median(subset2$n))
-subset3 <- filter(subset2, subset2$n <= sizefilter)
-#Error Filter
-errorFilter <- min(mean(subset3$msep),median(subset3$msep))
-subset4 <- filter(subset3, subset3$msep < errorFilter)
-#AIC Filter
-AICFilter <- min(mean(subset4$aic),median(subset4$aic))
-subset5 <- filter(subset4, subset4$aic < AICFilter)
-#SBC and SBIC 
-SBICFilter <- min(mean(subset5$sbic),median(subset5$sbic))
-SBCFilter <- min(mean(subset5$sbc),median(subset5$sbc))
-subset6<- filter(subset5, subset5$sbc < SBCFilter & subset5$sbic <= SBICFilter)
-#APC Filter Higher is better
-#https://olsrr.rsquaredacademy.com/reference/ols_apc.html
-APCFilter <- max(mean(subset6$apc),median(subset6$apc))
-subset7<- filter(subset6, subset6$apc > APCFilter)
-#Hocking's SP, lower is better
-#https://rdrr.io/cran/olsrr/man/ols_hsp.html
-#Average prediction mean squared error
-HSPFilter <- min(mean(subset7$hsp),median(subset7$hsp))
-subset8<- filter(subset7, subset7$hsp < HSPFilter)
-#Final Prediction Error
-FPEFilter <- min(mean(subset8$fpe),median(subset8$fpe))
-subset9<- filter(subset8, subset8$fpe < FPEFilter)
-#filtered
+
 View(subset9)
 print(subset9)
 
@@ -261,7 +320,7 @@ for (i in subset9$predictors) {
   print(names)
   
   trainingModel <- lm(train_xy_set[names])
-  fit <- lm(trainingModel, data=test_xy_set[names])
+  fit <- lm(trainingModel, data=test1_xy_set[names])
   
   print(summary(fit))
   print(RMSE(fit))
