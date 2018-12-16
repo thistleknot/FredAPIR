c4609c6da02236b74a4e2091ac5eedfa906ec96b
thistleknot <laferrierejc@gmail.com>
2018-12-16 04:39:03 +0000

    setup 10 cv exclusively bootstrapped/sampled exhaustive lists that are merged on their common significant pair combinations which will then be passed for cv testing

--- regression_analysis.R
+++ regression_analysis.R
@@ -61,154 +61,156 @@ nrow(y)
 ## 75% of the sample size
 #single split
 ## set the seed to make your partition reproducible
-set.seed(123)
 
 #used for exhaustive lists (oversamples)
 #possible to define two splits?
 
 #alternative is 75% bootstraps of resampled data, having it split into 1/3's makes the data diverse for 6 years
 #limit training size to 1/3? (6 years)
-smp_size <- floor(1/3 * nrow(MyData))
-#https://stackoverflow.com/questions/14864275/randomize-w-no-repeats-using-r
-training <- sample(nrow(MyData), replace=F)
-training1 <- training[1:smp_size]
-training2 <- training[(smp_size+1):(smp_size*2)]
-training3 <- training[((smp_size*2)+1):((smp_size*3))]
 
-#http://r-statistics.co/Linear-Regression.html
-#used to build models on new ranodmized data and then tested against holdout test data (to include cross validation)
-#not limited to just the first portion of the percent
-#provides an index
-
+smp_size <- floor(1/2 * nrow(MyData))
 vld_size <- floor(1.0 * nrow(MyData))
+#https://stackoverflow.com/questions/14864275/randomize-w-no-repeats-using-r
 
-training1Data <- c()
-training2Data <- c()
-training3Data <- c()
+set.seed(123)
+for (i in 1:5)
+{
+  training <- sample(nrow(MyData), replace=F)
+  training1 <- training[1:smp_size]
+  training2 <- training[(smp_size+1):nrow(MyData)]
+  
+  #http://r-statistics.co/Linear-Regression.html
+  #used to build models on new ranodmized data and then tested against holdout test data (to include cross validation)
+  #not limited to just the first portion of the percent
+  #provides an index
+  
+  training1Data <- c()
+  training2Data <- c()
+  
+  #new plan is to create two randomized partitions that will have complete dataset algo's done on them.
+  training1Data <- MyData[training1, ]  # model training data
+  training2Data <- MyData[training2, ]  # model training data
+  
+  train1_xy_set <- c()
+  train2_xy_set <- c()
+  
+  train1_xy_set <- training1Data[c(yField,xList)]
+  train2_xy_set <- training2Data[c(yField,xList)]
+  
+  names <- c()
+  vars <- c()
+  names <- c(names, 'yFYield_CSUSHPINSA')
+  #SPSSReducedModel <- c('yFYield_CSUSHPINSA','Q1','Q2','Q3','Q4','xYield_CASTHPI','xYield_CPALTT01USQ657N','xYield_GS10','xYield_MSPNHSUS','xYield_MVLOAS','xYield_NYXRSA','xYield_POP','xYield_POPTHM',xYield_SDXRSA','xYield_TB3MS','xYield_UMCSENT','xYield_USSLIND')
+  #removed xYield_POP due to high correlation with xYield_POPTHM as well as Q2 for collinearity reasons
+  #trainingModel <- lm(yFYield_CSUSHPINSA ~ Q1 + Q3 + Q4 + xYield_CASTHPI + xYield_CPALTT01USQ657N + xYield_GS10 + xYield_MSPNHSUS + xYield_MVLOAS + xYield_NYXRSA + xYield_POPTHM + xYield_SDXRSA + xYield_TB3MS + xYield_UMCSENT + xYield_USSLIND, data = train_xy_set)
+  vars <- c('Q1','Q3','Q4','xYield_CASTHPI','xYield_CPALTT01USQ657N','xYield_GS10','xYield_MSPNHSUS','xYield_MVLOAS','xYield_NYXRSA','xYield_POPTHM','xYield_SDXRSA','xYield_TB3MS','xYield_UMCSENT','xYield_USSLIND')
+  names <- c(names, vars)
+  
+  #doesn't throw an error here, but when I try to pass this model into ols_step_all_possible, it throws an error
+  #.null used with stepwise
+  #training1Model.null = ~lm (yFYield_CSUSHPINSA ~ 1, data = train1_xy_set)
+  training1Model <- lm(yFYield_CSUSHPINSA ~ Q1 + Q3 + Q4 + xYield_CASTHPI + xYield_CPALTT01USQ657N + xYield_GS10 + xYield_MSPNHSUS + xYield_MVLOAS + xYield_NYXRSA + xYield_POPTHM + xYield_SDXRSA + xYield_TB3MS + xYield_UMCSENT + xYield_USSLIND, data = train1_xy_set)
+  #training2Model.null = ~lm (yFYield_CSUSHPINSA ~ 1, data = train2_xy_set)
+  training2Model <- lm(yFYield_CSUSHPINSA ~ Q1 + Q3 + Q4 + xYield_CASTHPI + xYield_CPALTT01USQ657N + xYield_GS10 + xYield_MSPNHSUS + xYield_MVLOAS + xYield_NYXRSA + xYield_POPTHM + xYield_SDXRSA + xYield_TB3MS + xYield_UMCSENT + xYield_USSLIND, data = train2_xy_set)
+  
+  #obj1 = step(training1Model.null, scope=list(lower=training1Model.null, upper=training1Model), direction='forward')
+  #obj2 = step(training2Model.null, scope=list(lower=training1Model.null, upper=training2Model), direction='forward')
+  
+  olsrr::ols_plot_resid_stud_fit(training1Model)
+  layout(matrix(c(1,2,3,4),2,2))
+  olsrr::ols_plot_resid_stud_fit(training2Model)
+  layout(matrix(c(1,2,3,4),2,2))
+  
+  anova(training1Model,training2Model)
+  
+  summary(training1Model)
+  summary(training2Model)
+  
+  #****
+  #careful, takes a long time
+  resultsAAll <- ols_step_all_possible(training1Model, p=.05)
+  resultsBAll <- ols_step_all_possible(training2Model, p=.05)
+  #careful, takes a long time
+  #****
+  
+  #Adj R^2 Filter
+  adjR_Afilter <-  max(mean(resultsAAll$adjr),median(resultsAAll$adjr))
+  adjR_Bfilter <-  max(mean(resultsBAll$adjr),median(resultsBAll$adjr))
+  
+  #Mallows' Cp-statistic estimates the size of the bias that is introduced into the predicted responses by having an underspecified model.
+  # Mallows Distance from n Filter, lower absolute distance from n is better
+  #better to allow for more higher mallow's CP values since they are so disparate and no need to punish so heavily early on
+  #book says close to p+1
+  mcp_A_floor <- min(mean(resultsAAll$cp-resultsAAll$n+1),median(resultsAAll$cp-resultsAAll$n+1))
+  mcp_B_floor <- min(mean(resultsBAll$cp-resultsBAll$n+1),median(resultsBAll$cp-resultsBAll$n+1))
+  
+  #size filter
+  size_A_floor <- min(mean(resultsAAll$n),median(resultsAAll$n))
+  size_B_floor <- min(mean(resultsBAll$n),median(resultsBAll$n))
+  
+  #Error Filter
+  error_AFilter <- min(mean(resultsAAll$msep),median(resultsAAll$msep))
+  error_BFilter <- min(mean(resultsBAll$msep),median(resultsBAll$msep))
+  
+  #AIC Filter
+  AIC_AFilter <- min(mean(resultsAAll$aic),median(resultsAAll$aic))
+  AIC_BFilter <- min(mean(resultsBAll$aic),median(resultsBAll$aic))
+  
+  #SBC and SBIC 
+  SBIC_AFilter <- min(mean(resultsAAll$sbic),median(resultsAAll$sbic))
+  SBC_AFilter <- min(mean(resultsAAll$sbc),median(resultsAAll$sbc))
+  SBIC_BFilter <- min(mean(resultsBAll$sbic),median(resultsBAll$sbic))
+  SBC_BFilter <- min(mean(resultsBAll$sbc),median(resultsBAll$sbc))
+  
+  #APC Filter Higher is better
+  #https://olsrr.rsquaredacademy.com/reference/ols_apc.html
+  #Amemiya's Prediction Criterion penalizes R-squared more heavily than does adjusted R-squared for each addition degree of freedom used on the right-hand-side of the equation. 
+  #The higher the better for this criterion.
+  APC_AFilter <- max(mean(resultsAAll$apc),median(resultsAAll$apc))
+  APC_BFilter <- max(mean(resultsBAll$apc),median(resultsBAll$apc))
+  #& (resultsAAll$apc > APC_AFilter)
+  #decisive, heavily penalizes per factor
+  
+  #Hocking's SP, lower is better
+  #https://rdrr.io/cran/olsrr/man/ols_hsp.html
+  HSP_AFilter <- min(mean(resultsAAll$hsp),median(resultsAAll$hsp))
+  HSP_BFilter <- min(mean(resultsBAll$hsp),median(resultsBAll$hsp))
+  
+  #Average prediction mean squared error
+  #Final Prediction Error
+  FPE_AFilter <- min(mean(resultsAAll$fpe),median(resultsAAll$fpe))
+  FPE_BFilter <- min(mean(resultsBAll$fpe),median(resultsBAll$fpe))
+  
+  #filtered
+  #AIC and BIC hold the same interpretation in terms of model comparison. That is, the larger difference in either AIC or BIC indicates stronger evidence for one model over the other 
+  #(the lower the better). It's just the the AIC doesn't penalize the number of parameters as strongly as BIC.Jan 7, 2014
+  
+  #results in none... but does not change final outcome when inverted, so removed
+  #(resultsAAll$apc > APC_AFilter) &
+  subsetA <- filter(resultsAAll,  (resultsAAll$msep < error_AFilter) & (resultsAAll$adjr > adjR_Afilter)  & ((resultsAAll$cp-resultsAAll$n) <= mcp_A_floor) & (resultsAAll$n < size_A_floor)& (resultsAAll$aic < AIC_AFilter) & (resultsAAll$sbc < SBC_AFilter) & (resultsAAll$sbic < SBIC_AFilter)  & (resultsAAll$hsp < HSP_AFilter) & (resultsAAll$fpe < FPE_AFilter)) 
+  subsetB <- filter(resultsBAll,  (resultsBAll$msep < error_BFilter) & (resultsBAll$adjr > adjR_Bfilter)  & ((resultsBAll$cp-resultsBAll$n) <= mcp_B_floor) & (resultsBAll$n < size_B_floor)& (resultsBAll$aic < AIC_BFilter) & (resultsBAll$sbc < SBC_BFilter) & (resultsBAll$sbic < SBIC_BFilter)  & (resultsBAll$hsp < HSP_BFilter) & (resultsBAll$fpe < FPE_BFilter))
+  
+  max(subsetA$n,subsetB$n)
+  
+  #merge quickly (not merge function)  
+  
+  #1st pass merge a and b
+  if(i==1)
+  {
+    factor_test_list <- intersect(subsetA$predictors,subsetB$predictors)
+    
+  }
+  #subsequent passes, merge with each list separately
+  else
+  {
+    factor_test_list <- intersect(subsetA$predictors,factor_test_list)
+    factor_test_list <- intersect(subsetB$predictors,factor_test_list)
+  }
+  View(factor_test_list)
+}
 
-#before I was using exactly half the data, now I'm using random sampling over the sample dataset and expecting overprovisioning to occur
-#ensures the model pulls from the other data
-#this makes my pooled datasets sourced from smaller data pools than the validation pool (akin to encryption: reverse randomized partitioning, allows for multiple partitions to be layered over a disk that holds data))
-training1Data <- MyData[training1, ]  # model training data
-training2Data <- MyData[training2, ]  # model training data
-training3Data <- MyData[training2, ]  # model training data
 
-train1_xy_set <- c()
-train2_xy_set <- c()
-train3_xy_set <- c()
 
-train1_xy_set <- training1Data[c(yField,xList)]
-train2_xy_set <- training2Data[c(yField,xList)]
-train3_xy_set <- training3Data[c(yField,xList)]
-
-names <- c()
-vars <- c()
-names <- c(names, 'yFYield_CSUSHPINSA')
-#SPSSReducedModel <- c('yFYield_CSUSHPINSA','Q1','Q2','Q3','Q4','xYield_CASTHPI','xYield_CPALTT01USQ657N','xYield_GS10','xYield_MSPNHSUS','xYield_MVLOAS','xYield_NYXRSA','xYield_POP','xYield_POPTHM',xYield_SDXRSA','xYield_TB3MS','xYield_UMCSENT','xYield_USSLIND')
-#removed xYield_POP due to high correlation with xYield_POPTHM as well as Q2 for collinearity reasons
-#trainingModel <- lm(yFYield_CSUSHPINSA ~ Q1 + Q3 + Q4 + xYield_CASTHPI + xYield_CPALTT01USQ657N + xYield_GS10 + xYield_MSPNHSUS + xYield_MVLOAS + xYield_NYXRSA + xYield_POPTHM + xYield_SDXRSA + xYield_TB3MS + xYield_UMCSENT + xYield_USSLIND, data = train_xy_set)
-vars <- c('Q1','Q3','Q4','xYield_CASTHPI','xYield_CPALTT01USQ657N','xYield_GS10','xYield_MSPNHSUS','xYield_MVLOAS','xYield_NYXRSA','xYield_POPTHM','xYield_SDXRSA','xYield_TB3MS','xYield_UMCSENT','xYield_USSLIND')
-names <- c(names, vars)
-
-#doesn't throw an error here, but when I try to pass this model into ols_step_all_possible, it throws an error
-training1Model <- lm(yFYield_CSUSHPINSA ~ Q1 + Q3 + Q4 + xYield_CASTHPI + xYield_CPALTT01USQ657N + xYield_GS10 + xYield_MSPNHSUS + xYield_MVLOAS + xYield_NYXRSA + xYield_POPTHM + xYield_SDXRSA + xYield_TB3MS + xYield_UMCSENT + xYield_USSLIND, data = train1_xy_set)
-training2Model <- lm(yFYield_CSUSHPINSA ~ Q1 + Q3 + Q4 + xYield_CASTHPI + xYield_CPALTT01USQ657N + xYield_GS10 + xYield_MSPNHSUS + xYield_MVLOAS + xYield_NYXRSA + xYield_POPTHM + xYield_SDXRSA + xYield_TB3MS + xYield_UMCSENT + xYield_USSLIND, data = train2_xy_set)
-training3Model <- lm(yFYield_CSUSHPINSA ~ Q1 + Q3 + Q4 + xYield_CASTHPI + xYield_CPALTT01USQ657N + xYield_GS10 + xYield_MSPNHSUS + xYield_MVLOAS + xYield_NYXRSA + xYield_POPTHM + xYield_SDXRSA + xYield_TB3MS + xYield_UMCSENT + xYield_USSLIND, data = train3_xy_set)
-
-layout(matrix(c(1,2,3,4),2,2))
-olsrr::ols_plot_resid_stud_fit(training1Model)
-layout(matrix(c(1,2,3,4),2,2))
-olsrr::ols_plot_resid_stud_fit(training2Model)
-layout(matrix(c(1,2,3,4),2,2))
-olsrr::ols_plot_resid_stud_fit(training3Model)
-
-anova(training1Model,training2Model,training3Model)
-
-summary(training1Model)
-summary(training2Model)
-summary(training3Model)
-
-#****
-#careful, takes a long time
-resultsAAll <- ols_step_all_possible(training1Model, p=.05)
-resultsBAll <- ols_step_all_possible(training2Model, p=.05)
-resultsCAll <- ols_step_all_possible(training3Model, p=.05)
-#careful, takes a long time
-#****
-
-#Adj R^2 Filter
-adjR_Afilter <-  max(mean(resultsAAll$adjr),median(resultsAAll$adjr))
-adjR_Bfilter <-  max(mean(resultsBAll$adjr),median(resultsBAll$adjr))
-adjR_Cfilter <-  max(mean(resultsCAll$adjr),median(resultsCAll$adjr))
-
-#Mallows' Cp-statistic estimates the size of the bias that is introduced into the predicted responses by having an underspecified model.
-# Mallows Distance from n Filter, lower absolute distance from n is better
-#better to allow for more higher mallow's CP values since they are so disparate and no need to punish so heavily early on
-#book says close to p+1
-mcp_A_floor <- min(mean(resultsAAll$cp-resultsAAll$n+1),median(resultsAAll$cp-resultsAAll$n+1))
-mcp_B_floor <- min(mean(resultsBAll$cp-resultsBAll$n+1),median(resultsBAll$cp-resultsBAll$n+1))
-mcp_C_floor <- min(mean(resultsCAll$cp-resultsCAll$n+1),median(resultsCAll$cp-resultsCAll$n+1))
-
-#size filter
-size_A_floor <- min(mean(resultsAAll$n),median(resultsAAll$n))
-size_B_floor <- min(mean(resultsBAll$n),median(resultsBAll$n))
-size_C_floor <- min(mean(resultsCAll$n),median(resultsCAll$n))
-
-#Error Filter
-error_AFilter <- min(mean(resultsAAll$msep),median(resultsAAll$msep))
-error_BFilter <- min(mean(resultsBAll$msep),median(resultsBAll$msep))
-error_CFilter <- min(mean(resultsCAll$msep),median(resultsCAll$msep))
-
-#AIC Filter
-AIC_AFilter <- min(mean(resultsAAll$aic),median(resultsAAll$aic))
-AIC_BFilter <- min(mean(resultsBAll$aic),median(resultsBAll$aic))
-AIC_CFilter <- min(mean(resultsCAll$aic),median(resultsCAll$aic))
-
-#SBC and SBIC 
-SBIC_AFilter <- min(mean(resultsAAll$sbic),median(resultsAAll$sbic))
-SBC_AFilter <- min(mean(resultsAAll$sbc),median(resultsAAll$sbc))
-SBIC_BFilter <- min(mean(resultsBAll$sbic),median(resultsBAll$sbic))
-SBC_BFilter <- min(mean(resultsBAll$sbc),median(resultsBAll$sbc))
-SBIC_CFilter <- min(mean(resultsCAll$sbic),median(resultsCAll$sbic))
-SBC_CFilter <- min(mean(resultsCAll$sbc),median(resultsCAll$sbc))
-
-#APC Filter Higher is better
-#https://olsrr.rsquaredacademy.com/reference/ols_apc.html
-#Amemiya's Prediction Criterion penalizes R-squared more heavily than does adjusted R-squared for each addition degree of freedom used on the right-hand-side of the equation. 
-#The higher the better for this criterion.
-APC_AFilter <- max(mean(resultsAAll$apc),median(resultsAAll$apc))
-APC_BFilter <- max(mean(resultsBAll$apc),median(resultsBAll$apc))
-APC_CFilter <- max(mean(resultsCAll$apc),median(resultsCAll$apc))
-#& (resultsAAll$apc > APC_AFilter)
-#decisive, heavily penalizes per factor
-
-#Hocking's SP, lower is better
-#https://rdrr.io/cran/olsrr/man/ols_hsp.html
-HSP_AFilter <- min(mean(resultsAAll$hsp),median(resultsAAll$hsp))
-HSP_BFilter <- min(mean(resultsBAll$hsp),median(resultsBAll$hsp))
-HSP_CFilter <- min(mean(resultsCAll$hsp),median(resultsCAll$hsp))
-
-#Average prediction mean squared error
-#Final Prediction Error
-FPE_AFilter <- min(mean(resultsAAll$fpe),median(resultsAAll$fpe))
-FPE_BFilter <- min(mean(resultsBAll$fpe),median(resultsBAll$fpe))
-FPE_CFilter <- min(mean(resultsCAll$fpe),median(resultsCAll$fpe))
-
-#filtered
-#AIC and BIC hold the same interpretation in terms of model comparison. That is, the larger difference in either AIC or BIC indicates stronger evidence for one model over the other 
-#(the lower the better). It's just the the AIC doesn't penalize the number of parameters as strongly as BIC.Jan 7, 2014
-
-#results in none... but does not change final outcome when inverted, so removed
-#(resultsAAll$apc > APC_AFilter) &
-subsetA <- filter(resultsAAll,  (resultsAAll$msep < error_AFilter) & (resultsAAll$adjr > adjR_Afilter)  & ((resultsAAll$cp-resultsAAll$n) <= mcp_A_floor) & (resultsAAll$n < size_A_floor)& (resultsAAll$aic < AIC_AFilter) & (resultsAAll$sbc < SBC_AFilter) & (resultsAAll$sbic < SBIC_AFilter)  & (resultsAAll$hsp < HSP_AFilter) & (resultsAAll$fpe < FPE_AFilter)) 
-subsetB <- filter(resultsBAll,  (resultsBAll$msep < error_BFilter) & (resultsBAll$adjr > adjR_Bfilter)  & ((resultsBAll$cp-resultsBAll$n) <= mcp_B_floor) & (resultsBAll$n < size_B_floor)& (resultsBAll$aic < AIC_BFilter) & (resultsBAll$sbc < SBC_BFilter) & (resultsBAll$sbic < SBIC_BFilter)  & (resultsBAll$hsp < HSP_BFilter) & (resultsBAll$fpe < FPE_BFilter))
-subsetC <- filter(resultsCAll,  (resultsCAll$msep < error_CFilter) & (resultsCAll$adjr > adjR_Cfilter)  & ((resultsCAll$cp-resultsCAll$n) <= mcp_C_floor) & (resultsCAll$n < size_C_floor)& (resultsCAll$aic < AIC_CFilter) & (resultsCAll$sbc < SBC_CFilter) & (resultsCAll$sbic < SBIC_CFilter)  & (resultsCAll$hsp < HSP_CFilter) & (resultsCAll$fpe < FPE_CFilter))
-
-max(subsetA$n,subsetB$n,subsetC$n)
-
-#merge quickly (not merge function)
-factor_test_list <- intersect(intersect(subsetA$predictors,subsetB$predictors),subsetC$predictors)
-View(factor_test_list)
 
 #https://www.analyticsvidhya.com/blog/2018/05/improve-model-performance-cross-validation-in-python-r/
 #cross validation
@@ -399,8 +401,14 @@ for (i in seq(factor_test_list))
 RMSE_error_Filter = sd(as.numeric(as.character(cv_model10_log$RMSE)))+min(as.numeric(as.character(cv_model10_log$RMSE)))
 
 topPicks <- filter(cv_model10_log,  (as.numeric(as.character(cv_model10_log$RMSE)) < RMSE_error_Filter))
-hist((as.numeric(as.character(cv_model10_log$RMSE)) ))
-hist((as.numeric(as.character(topPicks$RMSE)) ))
+
+par(mfrow=2:1)
+{
+
+  hist((as.numeric(as.character(cv_model10_log$RMSE))))
+  hist((as.numeric(as.character(topPicks$RMSE))))
+}
+
 colnames(topPicks) <- cnames
 #trying to order by RMSE
 
