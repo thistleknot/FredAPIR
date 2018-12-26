da48081571a5bf7d7e248860832543010becf053
thistleknot <laferrierejc@gmail.com>
2018-12-26 17:34:27 +0000

    trying to setup knn

--- regression_analysis.R
+++ regression_analysis.R
@@ -33,6 +33,15 @@ predict.regsubsets = function(object, newdata, id, ...) {
   mat[, names(coefi)] %*% coefi
 }
 
+knn.reg.bestK = function(Xtrain, Xtest, ytrain, ytest, kmax=20) {
+  vec.rmse = rep(NA, kmax)
+  for (k in 1:kmax) {
+    yhat.test = knn.reg(Xtrain, Xtest, ytrain, k)$pred
+    vec.rmse[k] = rmse(yhat.test, ytest)
+  }
+  list(k.opt = which.min(vec.rmse), rmse.min = min(vec.rmse), vec.rmse)
+}
+
 #https://onlinecourses.science.psu.edu/stat501/node/334/
 #RSS
 PRESS <- function(linear.model) {
@@ -415,101 +424,39 @@ bestSize = which(mean.cv.errors %in% min(mean.cv.errors))
 
 bestsubset = regsubsets(yFYield_CSUSHPINSA ~ ., data = subset, nbest=1, nvmax=bestSize, method=c("exhaustive"))
 
+#https://rstudio-pubs-static.s3.amazonaws.com/2897_9220b21cfc0c43a396ff9abf122bb351.html
+plot(bestsubset, scale = "adjr2", main = "Adjusted R^2")
+
 finish = coef(bestsubset, bestSize)
+
+cv.errors=matrix(NA,divisions,,)
+
+for(j in 1:divisions){
+  #best.fit = regsubsets(yFYield_CSUSHPINSA ~., data=subset[folds != j,], nvmax = length(factor_test_list)-1)
+  knn_model <- knn.reg.bestK(subset[folds == j,], subset[folds != j,], subset[folds == j,]$yFYield_CSUSHPINSA, subset[folds != j,]$yFYield_CSUSHPINSA)
+  #predictions
+  #knn.reg(dat[id.train, ], test = dat[id.test, ], dat$yFYield_CSUSHPINSA[id.train], k = knn_model$opt)
+  #for (i in 1:(length(factor_test_list)-1)){
+    #pred = predict.regsubsets(best.fit, subset[folds == j, ], id = i)  
+    yhat = knn.reg(subset[folds == j,], subset[folds != j,], subset[folds == j,]$yFYield_CSUSHPINSA, knn_model$k.opt)
+    yhat.test = rep(0, length(id.test))
+    yhat.test[yhat$pred > 0] = 1
+    print(yhat.test)
+    #MSE
+    #cv.errors[j, i] = mean((subset$yFYield_CSUSHPINSA[folds == j] - pred)^2)
+    #cv.errors[j, i] = mean(yhat.test != dat2$BL_yFYield_CSUSHPINSA[id.test])
+    cv.errors[j] = mean(yhat.test != MyData[folds != j,]$BL_yFYield_CSUSHPINSA)
+  #}
+}
+
+
+
+
+
+#kNNdistplot(dat[id.train, ], k=knn_model$k.opt)
+
+
 print(finish)
 #plot(subsets)
 View(finish)
 write.csv(finish, "coefficients.csv")
-
-
-#used for comprehensive lists
-{
-  
-  #Adj R^2 Filter
-  adjR_Afilter <-  max(mean(resultsAll1$adjr),median(resultsAll1$adjr))
-  adjR_Bfilter <-  max(mean(resultsAll2$adjr),median(resultsAll2$adjr))
-  
-  #Mallows' Cp-statistic estimates the size of the bias that is introduced into the predicted responses by having an underspecified model.
-  # Mallows Distance from n Filter, lower absolute distance from n is better
-  #better to allow for more higher mallow's CP values since they are so disparate and no need to punish so heavily early on
-  #book says close to p+1
-  mcp_A_floor <- min(mean(resultsAll1$cp-resultsAll1$n+1),median(resultsAll1$cp-resultsAll1$n+1))
-  mcp_B_floor <- min(mean(resultsAll2$cp-resultsAll2$n+1),median(resultsAll2$cp-resultsAll2$n+1))
-  
-  #size filter
-  size_A_floor <- min(mean(resultsAll1$n),median(resultsAll1$n))
-  size_B_floor <- min(mean(resultsAll2$n),median(resultsAll2$n))
-  
-  #Error Filter
-  error_AFilter <- min(mean(resultsAll1$msep),median(resultsAll1$msep))
-  #error_AFilter <- min(mean(resultsAll1$rmse),median(resultsAll1$rmse))
-  error_BFilter <- min(mean(resultsAll2$msep),median(resultsAll2$msep))
-  #error_BFilter <- min(mean(resultsAll2$rmse),median(resultsAll2$rmse))
-  
-  #AIC Filter
-  AIC_AFilter <- min(mean(resultsAll1$aic),median(resultsAll1$aic))
-  AIC_BFilter <- min(mean(resultsAll2$aic),median(resultsAll2$aic))
-  
-  #SBC and SBIC 
-  #SBIC_AFilter <- min(mean(resultsAll1$sbic),median(resultsAll1$sbic))
-  SBC_AFilter <- min(mean(resultsAll1$sbc),median(resultsAll1$sbc))
-  #SBIC_BFilter <- min(mean(resultsAll2$sbic),median(resultsAll2$sbic))
-  SBC_BFilter <- min(mean(resultsAll2$sbc),median(resultsAll2$sbc))
-  
-  #APC Filter Higher is better
-  #https://olsrr.rsquaredacademy.com/reference/ols_apc.html
-  #Amemiya's Prediction Criterion penalizes R-squared more heavily than does adjusted R-squared for each addition degree of freedom used on the right-hand-side of the equation. 
-  #The higher the better for this criterion.
-  APC_AFilter <- max(mean(resultsAll1$apc),median(resultsAll1$apc))
-  APC_BFilter <- max(mean(resultsAll2$apc),median(resultsAll2$apc))
-  #& (resultsAAll$apc > APC_AFilter)
-  #decisive, heavily penalizes per factor
-  
-  #Hocking's SP, lower is better
-  #https://rdrr.io/cran/olsrr/man/ols_hsp.html
-  HSP_AFilter <- min(mean(resultsAll1$hsp),median(resultsAll1$hsp))
-  HSP_BFilter <- min(mean(resultsAll2$hsp),median(resultsAll2$hsp))
-  
-  #Average prediction mean squared error
-  #Final Prediction Error
-  #doesnt work with backwards
-  FPE_AFilter <- min(mean(resultsAll1$fpe),median(resultsAll1$fpe))
-  FPE_BFilter <- min(mean(resultsAll2$fpe),median(resultsAll2$fpe))
-  
-  #filtered
-  #AIC and BIC hold the same interpretation in terms of model comparison. That is, the larger difference in either AIC or BIC indicates stronger evidence for one model over the other 
-  #(the lower the better). It's just the the AIC doesn't penalize the number of parameters as strongly as BIC.Jan 7, 2014
-  
-  #results in none... but does not change final outcome when inverted, so removed
-  #(resultsAAll$apc > APC_AFilter) &
-  #subsetA <- filter(resultsAAll,  (resultsAAll$msep < error_AFilter) & (resultsAAll$adjr > adjR_Afilter)  & ((resultsAAll$cp-resultsAAll$n) <= mcp_A_floor) & (resultsAAll$n < size_A_floor)& (resultsAAll$aic < AIC_AFilter) & (resultsAAll$sbc < SBC_AFilter) & (resultsAAll$sbic < SBIC_AFilter)  & (resultsAAll$hsp < HSP_AFilter) & (resultsAAll$fpe < FPE_AFilter)) 
-  #subsetB <- filter(resultsBAll,  (resultsBAll$msep < error_BFilter) & (resultsBAll$adjr > adjR_Bfilter)  & ((resultsBAll$cp-resultsBAll$n) <= mcp_B_floor) & (resultsBAll$n < size_B_floor)& (resultsBAll$aic < AIC_BFilter) & (resultsBAll$sbc < SBC_BFilter) & (resultsBAll$sbic < SBIC_BFilter)  & (resultsBAll$hsp < HSP_BFilter) & (resultsBAll$fpe < FPE_BFilter))
-  
-  #resultsAAll$
-  #& (resultsAll1$fpe < FPE_AFilter)
-  subsetA <- filter(resultsAll1,  (resultsAll1$msep < error_AFilter) & (resultsAll1$adjr > adjR_Afilter)  & ((resultsAll1$cp-resultsAll1$n) <= mcp_A_floor) & (resultsAll1$n < size_A_floor)& (resultsAll1$aic < AIC_AFilter) &(resultsAll1$sbc < SBC_AFilter)  & (resultsAll1$hsp < HSP_AFilter) & (resultsAll1$fpe < FPE_AFilter) ) 
-  subsetB <- filter(resultsAll2,  (resultsAll2$msep < error_BFilter) & (resultsAll2$adjr > adjR_Bfilter)  & ((resultsAll2$cp-resultsAll2$n) <= mcp_B_floor) & (resultsAll2$n < size_B_floor)& (resultsAll2$aic < AIC_BFilter) & (resultsAll2$sbc < SBC_BFilter)  & (resultsAll2$hsp < HSP_BFilter) & (resultsAll2$fpe < FPE_BFilter)) 
-  
-}
-
-#f_list <- intersect(subsetA$predictors,subsetB$predictors)
-
-if (length(f_list)==0)
-{
-  f_list <- intersect(resultsAll1$predictors,resultsAll2$predictors)
-}
-View(f_list)
-
-#https://www.analyticsvidhya.com/blog/2018/05/improve-model-performance-cross-validation-in-python-r/
-#cross validation
-#rf = random forest
-#nb = naive bayes
-#method="rf"
-#train_control <- trainControl(method="cv", number=10)
-#model <- train(yFYield_CSUSHPINSA~., data=train_xy_set, trControl=train_control, contrasts = NULL)
-
-#https://stats.stackexchange.com/questions/61090/how-to-split-a-data-set-to-do-10-fold-cross-validation
-#flds <- createFolds(y, k = 10, list = TRUE, returnTrain = FALSE)
-#names(flds)[1] <- "train"
-
-write.csv(factor_test_list,"factor_test_list.csv")
